e7d5dd7eab9fe16ba6de56d79b7abe63bd96b75b
Murray Campbell <murrayirc@gmail.com>
2018-11-02 12:26:34 -0500

    Moved stuff around, re enabled old input

--- Assets/Common/Code/Managers/InputManager.cs
+++ Assets/Common/Code/Managers/InputManager.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Input;
 
 public class InputManager : Manager {
+    public Vector2 MouseLook { get; private set; }
+
 	public override void Startup() {
 		managerState = ManagerState.Started;
+    }
+
+    private void Update() {
+        //MouseLook = new Vector2(, Mouse.current.delta.y.);
     }
 
 	public override void Shutdown() {
